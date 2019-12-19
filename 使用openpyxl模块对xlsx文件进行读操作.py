d91ef5eb2b7ed7c26e2fb85b31584928c0b85172
lansejingyu <18710512504@163.com>
2019-12-19 14:00:38 +0800

    添加代码

---
+++ 使用openpyxl模块对xlsx文件进行读操作.py
@@ -0,0 +1,13 @@
+#使用openpyxl模块对xlsx文件进行读操作
+
+'''
+xlrd和xlwt都是针对Excel97-2003操作的，也就是以xls结尾的文件。
+Excel2007以上的版本，以xlsx为后缀。要对这种类型的Excel文件进行操作要使用openpyxl，该模块既可以进行“读”操作，也可以进行“写”操作，还可以对已经存在的文件做修改。
+'''
+
+import openpyxl
+
+#获取工作簿对象
+workbook = openpyxl.load_workbook("C:\\Users\\EDZ\\Desktop\\1.xlsx")
+#与xlrd 模块的区别
+#wokrbook=xlrd.open_workbook("C:\\Users\\EDZ\\Desktop\\1.xlsx")
